fa08ae96aa4b5f878f060ab3ec92dea7e3ec5ffd
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-01-20 23:44:52 +0530

    Add program to concat two strings

---
+++ src/Strings/StringManupulation.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class StringManupulation {
+
+    public static void main(String[] args) {
+
+        String firstName = "Ayush";
+        String lastName = "Gunjal";
+        String fullname = firstName.concat(" ").concat(lastName);
+        
+        System.out.println(fullname.toUpperCase());
+
+    }
+
+}
