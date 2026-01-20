97fd5675a79a5b0957ab4ec2f2bc048d8f145833
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-01-20 23:52:29 +0530

    Add program to find square root of a number

---
+++ src/Math/SquareRootDemo.java
@@ -0,0 +1,19 @@
+package Math;
+
+import java.util.Scanner;
+
+public class SquareRootDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double num = sc.nextDouble();
+
+        double sqrt = Math.sqrt(num);
+
+        System.out.println("Square root of " + num + " is: " + sqrt);
+
+        sc.close();
+    }
+}
