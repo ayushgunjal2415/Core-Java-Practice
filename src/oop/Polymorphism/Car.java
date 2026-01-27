07e1038acec5aa8775b9012ba908e22d613c68f0
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-01-27 23:50:50 +0530

    Override service method in Car class using super keyword

---
+++ src/oop/Polymorphism/Car.java
@@ -0,0 +1,10 @@
+package oop.Polymorphism;
+
+ class Car extends Vehicle {
+    
+    @Override
+    public void service(){
+        super.service();
+        System.out.println("Car is getting serviced..");
+    }
+}
