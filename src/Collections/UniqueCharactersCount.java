edaa37965240761e13d347982926c0c3c3ca3ff8
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-15 00:50:01 +0530

    Created program to count unique characters in a string using HashSet

---
+++ src/Collections/UniqueCharactersCount.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class UniqueCharactersCount {
+
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        Set<Character> set = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            set.add(ch);
+        }
+
+        System.out.println("Unique characters: " + set);
+        System.out.println("Count of unique characters: " + set.size());
+    }
+}
