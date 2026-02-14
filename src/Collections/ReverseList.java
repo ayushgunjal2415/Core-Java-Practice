cd96a361d37068f5ad36e1d0b770d438dcb22a23
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-15 00:44:13 +0530

    Added list reversal program

---
+++ src/Collections/ReverseList.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ReverseList {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println("Original List: " + list);
+
+        Collections.reverse(list);
+
+        System.out.println("Reversed List: " + list);
+    }
+}
+
