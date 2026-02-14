6f3d012d9cc2d50aaab1c0b54d4e33912083940f
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-15 00:47:18 +0530

    Implemented method to swap two elements in ArrayList using given indices

---
+++ src/Collections/SwapElements.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwapElements {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(9);
+
+        System.out.println(list);
+        swap(list, 1, 3);
+        System.out.println(list);
+    }
+
+    public static void swap(List<Integer> list, int x, int y){
+        int swap = list.get(x);
+        list.set(x, list.get(y));
+        list.set(y, swap);
+
+    }
+}
