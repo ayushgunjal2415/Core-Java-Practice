84f3f38a8a5f70914ca568a355136ab657e9e29b
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-15 00:46:25 +0530

    Added frequency counter using Collections class

---
+++ src/Collections/FrequencyCounter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FrequencyCounter {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("Java");
+        list.add("C++");
+        list.add("Java");
+
+        System.out.println(list);
+
+        int frequency = Collections.frequency(list, "Java");
+        int frequency1 = Collections.frequency(list, "C++");
+
+        System.out.println("Frequency of Java: " + frequency);
+        System.out.println("Frequency of C++: " + frequency1);
+    }
+}
