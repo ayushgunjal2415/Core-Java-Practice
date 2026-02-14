89b59b6f2a4ece29ccbbc45347b239a305fdf3d8
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-15 00:42:31 +0530

    Added string concatenation method using varargs

---
+++ src/Collections/ConcatenateStrings.java
@@ -0,0 +1,18 @@
+public class ConcatenateStrings {
+
+    // Method with varargs
+    public static String concatenate(String... strings) {
+        StringBuilder result = new StringBuilder();
+
+        for (String str : strings) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String output = concatenate("Hello ", "Java ", "World!");
+        System.out.println("Concatenated String: " + output);
+    }
+}
