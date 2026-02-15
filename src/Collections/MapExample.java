8397c2eb3c577d7b81849ba7f2d7a4a19580e51e
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-15 18:56:06 +0530

    Add HashMap example demonstrating basic Map operations

---
+++ src/Collections/MapExample.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample{
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Ayush", 21);
+        map.put("Aryan", 25);
+        map.put("Aaditya", 36);
+        System.out.println(map.size());
+        System.out.println(map.containsKey("Aaditya"));
+        System.out.println(map.remove("Aryan"));
+        System.out.println(map.get("Ayush"));
+        System.out.println(map.size());
+
+
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("Ayush", 21);
+        map2.put("Aaditya", 36);
+
+        System.out.println(map.equals(map2));
+        
+        System.out.println(map.hashCode());
+        System.out.println(map2.hashCode());
+
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+
+  
+}
