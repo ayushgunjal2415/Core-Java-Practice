c2ee6a7fc94cded654aca4371304b976058053f3
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-16 16:48:02 +0530

    Add Day enum with weekday/weekend classification

---
+++ src/Collections/Day.java
@@ -0,0 +1,20 @@
+public enum Day {
+    
+    MONDAY(true), 
+    TUESDAY(true), 
+    WEDNESDAY(true), 
+    THURSDAY(true),
+    FRIDAY(true), 
+    SATURDAY(false),
+    SUNDAY(false);
+
+    private final boolean isWeekday;
+
+     private Day(boolean isWeekday) {
+        this.isWeekday = isWeekday;
+    }
+    
+    public String getType(){
+        return isWeekday ? "Weekday" : "Weekend";
+    }
+}
