651e5c17820fbadec204eca98d3b00d0488edb6c
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-16 16:48:51 +0530

    Add TestEnum class to print all days with type

---
+++ src/Collections/TestEnum.java
@@ -0,0 +1,11 @@
+
+public class TestEnum{
+
+    public static void main(String[] args) {
+        System.out.println("Printing all days of the week..");
+
+        for (Day day : Day.values()) {
+            System.out.println(day+ ": " + day.getType());
+        }
+    }
+}
