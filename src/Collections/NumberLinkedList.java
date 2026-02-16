4efafc7187bc4cfc48c8166ff96ec8b6a2237993
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-16 16:53:39 +0530

    Add LinkedList example to remove first element

---
+++ src/Collections/NumberLinkedList.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class NumberLinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> numbers = new LinkedList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        numbers.removeFirst();
+
+        System.out.println("After removing first element:");
+        System.out.println(numbers);
+    }
+}
