e7d63d4f65e3bb92dd37bc0e0aa64b22dfcc5383
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-19 20:08:52 +0530

    Add TestTrafficLight to control signal sequence using join()

---
+++ src/Multithreading/TestTrafficLight.java
@@ -0,0 +1,21 @@
+package Multithreading;
+
+public class TestTrafficLight {
+    public static void main(String[] args) throws InterruptedException{
+        
+        TrafficLightThread red = new TrafficLightThread(TrafficColor.RED);
+        TrafficLightThread yellow = new TrafficLightThread(TrafficColor.YELLOW);
+        TrafficLightThread green = new TrafficLightThread(TrafficColor.GREEN);
+
+        green.start();
+        green.join();
+
+        yellow.start();
+        yellow.join();
+
+        red.start();
+        red.join();
+        
+
+    }
+}
