c0fb382f06a779ab0b2b3485a1b56fccb27659fb
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-19 20:01:53 +0530

    Add ThreadState example demonstrating thread states

---
+++ src/Multithreading/ThreadState.java
@@ -0,0 +1,14 @@
+package Multithreading;
+
+public class ThreadState extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(4000);
+            System.out.println("From inside run " + getState());
+        } catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
