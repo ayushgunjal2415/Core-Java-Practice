77278950266d62e5ceab75b66359d3a1405fa97c
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-20 23:52:02 +0530

    Add Callable example to calculate square using single thread executor

---
+++ src/Multithreading/SquareCalculator.java
@@ -0,0 +1,44 @@
+package Multithreading;
+
+import java.util.concurrent.*;
+
+class MyTask implements Callable<Integer> {
+
+    private int number;
+
+    public MyTask(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Thread.currentThread().getName() + 
+                           " is calculating square of " + number);
+        Thread.sleep(1000);
+        return number * number;
+    }
+}
+
+public class SquareCalculator {
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService service = Executors.newFixedThreadPool(1);
+
+        // Creating multiple Callable tasks
+        Callable<Integer> task1 = new MyTask(2);
+        Callable<Integer> task2 = new MyTask(4);
+        Callable<Integer> task3 = new MyTask(6);
+
+        // Submit tasks
+        Future<Integer> f1 = service.submit(task1);
+        Future<Integer> f2 = service.submit(task2);
+        Future<Integer> f3 = service.submit(task3);
+
+        // Get results
+        System.out.println("Result 1: " + f1.get());
+        System.out.println("Result 2: " + f2.get());
+        System.out.println("Result 3: " + f3.get());
+
+        service.shutdown();
+    }
+}
