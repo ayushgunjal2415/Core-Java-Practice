6139a1236c96034cfda4a864478726f8614892db
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-22 22:35:20 +0530

    Add example to convert String list to integers, square them, and sum using Stream API

---
+++ src/FunctionalProgramming/StringToIntegerSquare.java
@@ -0,0 +1,16 @@
+package FunctionalProgramming;
+
+import java.util.List;
+
+public class StringToIntegerSquare {
+    public static void main(String[] args) {
+        
+        List<String> list = List.of("1","2","3","4","5","6","7","8","9");
+
+        list.stream()
+        .map(Integer::parseInt)
+        .map(num -> Math.pow(num, 2))
+        .reduce(Double::sum)
+        .ifPresent(System.out::println);
+    }
+}
