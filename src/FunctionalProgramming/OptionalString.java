4f8a18718ca14a80e7e65d46773eb4de807e8137
Ayush Gunjal <247179965+ayushgunjal2415@users.noreply.github.com>
2026-02-22 22:31:14 +0530

    Implement Optional handling for null and empty string with uppercase transformation

---
+++ src/FunctionalProgramming/OptionalString.java
@@ -0,0 +1,20 @@
+package FunctionalProgramming;
+
+import java.util.Optional;
+
+public class OptionalString {
+    public static void main(String[] args) {
+        
+        System.out.println(tOptional(null));
+        System.out.println(tOptional(""));
+        System.out.println(tOptional("Ayush"));
+    }
+
+    public static Optional<String> tOptional(String str){
+
+        if (str == null || str.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(str.toUpperCase());
+    }
+}
